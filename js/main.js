ff2811f09f1cc3797a2c28b1ee09278809707790
Adrian Lazarov <adrianlazarov@MacBook-Pro-Adrian.local>
2020-08-02 20:19:40 +0300

    v0.0.97

--- js/main.js
+++ js/main.js
@@ -3,42 +3,42 @@ let dataProjects = [
       "id":"1",
       "title":"Турфирма Анастасия",
       "desc":"",
-      "img":"https://lazarovadrian.github.io/img/anastasiyasochi.jpg",
-      "link":"https://anastasiyasochi.ru/"
+      "img":"//lazarovadrian.github.io/img/anastasiyasochi.jpg",
+      "link":"//anastasiyasochi.ru/"
     },
     {
       "id":"2",
       "title":"Свадебный салон Кокетка",
       "desc":"",
-      "img":"https://lazarovadrian.github.io/img/koketka.png",
-      "link":"https://koketkadress.ru/"
+      "img":"//lazarovadrian.github.io/img/koketka.png",
+      "link":"//koketkadress.ru/"
     },
     {
       "id":"3",
       "title":"EKazakova Wedding Photographer",
       "desc":"",
-      "img":"https://lazarovadrian.github.io/img/ekazakova.png",
-      "link":"https://ekazakova.ru/"
+      "img":"//lazarovadrian.github.io/img/ekazakova.png",
+      "link":"//ekazakova.ru/"
     },
     {
       "id":"4",
       "title":"Аренда машин под такси",
       "desc":"",
-      "img":"https://lazarovadrian.github.io/img/arendataxi.png",
-      "link":"http://arendav.taxi/"
+      "img":"//lazarovadrian.github.io/img/arendataxi.png",
+      "link":"//arendav.taxi/"
     },
     {
       "id":"5",
       "title":"Фламена",
       "desc":"",
-      "img":"https://lazarovadrian.github.io/img/flamena.jpg",
-      "link":"http://www.flamena-a.ru/"
+      "img":"//lazarovadrian.github.io/img/flamena.jpg",
+      "link":"//www.flamena-a.ru/"
     },
     {
       "id":"6",
       "title":"Сергей Терешкин",
       "desc":"",
-      "img":"https://lazarovadrian.github.io/img/sergSite.png",
+      "img":"//lazarovadrian.github.io/img/sergSite.png",
       "link":"#"
     }
 ];
