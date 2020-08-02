eec67d193f032b185b1568155e17fa28bd4a090e
Adrian Lazarov <adrianlazarov@MacBook-Pro-Adrian.local>
2020-08-02 16:58:22 +0300

    v0.0.91

--- js/mian.js
+++ js/mian.js
@@ -1,31 +1,54 @@
 window.addEventListener("DOMContentLoaded", () => {
 
-        getResurce("https://lazarovadrian.github.io/js/projects.json")
-            .then(data => projects(data))
-            .catch(err => console.error(err))
+        let dataProjects = [
+            {
+             "id":"1",
+             "title":"Турфирма Анастасия",
+             "desc":"",
+             "img":"https://lazarovadrian.github.io/img/js_primer.jpg",
+             "link":"https://anastasiyasochi.ru/"
+           },
+           {
+             "id":"2",
+             "title":"Свадебный салон Кокетка",
+             "desc":"",
+             "img":"https://lazarovadrian.github.io/img/koketka.png",
+             "link":"https://koketkadress.ru/"
+           },
+           {
+             "id":"3",
+             "title":"EKazakova Wedding Photographer",
+             "desc":"",
+             "img":"https://lazarovadrian.github.io/img/ekazakova.png",
+             "link":"https://ekazakova.ru/"
+           },
+           {
+             "id":"4",
+             "title":"Аренда машин под такси",
+             "desc":"",
+             "img":"https://lazarovadrian.github.io/img/arendataxi.png",
+             "link":"http://arendav.taxi/"
+           },
+           {
+             "id":"5",
+             "title":"Фламена",
+             "desc":"",
+             "img":"https://lazarovadrian.github.io/img/flamena.png",
+             "link":"http://www.flamena-a.ru/"
+           }
+        ];
 
-        async function getResurce(url){
-            const res = await fetch(url,{
-                method: 'POST',
-                headers:{'Content-Type': 'application/json'}
-            });
-    
-            if(!res.ok){throw new Error(`not fetch ${url}, status ${res.status}`);}
-            return await res.json();
-        }
-
-        function projects(res){
-            res.forEach(i => {
-                let project = document.createElement('div');
-                    project.classList.add('item_project');
-                    project.innerHTML = `   
-                        <img src="${i.img}" alt="${i.title}" class="img"> 
-                        <div class="project-body">                            
-                            <a href="${i.link}" aria-label="${i.title}" target="_blank" class="title">${i.title}</a>
-                            <p class="desc">${i.desc}</p>
-                        </div>
-                    `;
-                document.querySelector('.list_projects').appendChild(project);
-            });
-        }    
+        dataProjects.forEach(i => {
+            let project = document.createElement('div');
+                project.classList.add('item_project');
+                project.innerHTML = `   
+                    <img src="${i.img}" alt="${i.title}" class="img"> 
+                    <div class="project-body">                            
+                        <a href="${i.link}" aria-label="${i.title}" target="_blank" class="title">${i.title}</a>
+                        <p class="desc">${i.desc}</p>
+                    </div>
+                `;
+            document.querySelector('.list_projects').appendChild(project);
+        });
+        
 });
